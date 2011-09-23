5322110ec66e8773490372c1834e64f4e0d4f95f
eddiemachado <machado08@gmail.com>
2011-09-23 18:10:51 -0500

    added new HBP header
    added new "oldie" class
    standard wp title tag for better integration
    removed icon calls
    referenced new stylesheets

--- header.php
+++ header.php
@@ -1,39 +1,50 @@
 <!doctype html>  
 
 <!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
-<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
-<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
-<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
+<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6 oldie"> <![endif]-->
+<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7 oldie"> <![endif]-->
+<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8 oldie"> <![endif]-->
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 	
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		
-		<title><?php wp_title('', true, 'right'); ?></title>
+		<title><?php
+		/*
+		 * Print the <title> tag based on what is being viewed.
+		 */
+		global $page, $paged;
+		wp_title( '-', true, 'right' );
+		// Add the blog name.
+		bloginfo( 'name' );
+		// Add a page number if necessary:
+		if ( $paged >= 2 || $page >= 2 )
+			echo ' - ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
+		?></title>
+		
 		<meta name="description" content="">
 		<meta name="author" content="">
+		
+		<!-- mobile optimized -->
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<!-- IE6 toolbar removal -->
+		<meta http-equiv="imagetoolbar" content="false" />
+		<!-- allow pinned sites -->
+		<meta name="application-name" content="<?php bloginfo('name'); ?>" />
+		
+		<!-- icons & favicons (for more: http://themble.com/support/adding-icons-favicons/) -->
+		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 
 		<!-- default stylesheet -->
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/css/default.css">
-		
-		<!-- icons & favicons -->
-		<!-- For iPhone 4 -->
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/h/apple-touch-icon.png">
-		<!-- For iPad 1-->
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/m/apple-touch-icon.png">
-		<!-- For iPhone 3G, iPod Touch and Android -->
-		<link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/l/apple-touch-icon-precomposed.png">
-		<!-- For Nokia -->
-		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/l/apple-touch-icon.png">
-		<!-- For everything else -->
-		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/css/normalize.css">		
 		
 		<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-		<script>window.jQuery || document.write(unescape('%3Cscript src="<?php echo get_template_directory_uri(); ?>/library/js/libs/jquery-1.6.1.min.js"%3E%3C/script%3E'))</script>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+		<script>window.jQuery || document.write(unescape('%3Cscript src="<?php echo get_template_directory_uri(); ?>/library/js/libs/jquery-1.6.2.min.js"%3E%3C/script%3E'))</script>
 		
-		<script src="<?php echo get_template_directory_uri(); ?>/library/js/modernizr-2.0.min.js"></script>
+		<!-- modernizr -->
+		<script src="<?php echo get_template_directory_uri(); ?>/library/js/modernizr.full.min.js"></script>
 		
   		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 		
@@ -41,7 +52,6 @@
 		<?php wp_head(); ?>
 		<!-- end of wordpress head -->
 		
-		<!-- stylesheet is called after wp_head so you can overwrite plugin styles if needed -->
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 		
 	</head>
