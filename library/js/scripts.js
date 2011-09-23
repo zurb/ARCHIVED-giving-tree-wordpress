d01f4f53cd38fa558d74698163229635db0e352e
eddiemachado <machado08@gmail.com>
2011-09-23 18:13:57 -0500

    added sample Modernizr test

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -9,6 +9,16 @@ slow the page load.
 
 */
 
+// Modernizr.load loading the right scripts only if you need them
+Modernizr.load([
+	{
+    // Let's see if we need to load selectivizr
+    test : Modernizr.borderradius,
+    // Modernizr.load loads selectivizr for IE6-8
+    nope : ['selectivizr-min.js']
+	}
+]);
+
 /* imgsizer (flexible images for fluid sites) */
 var imgSizer={Config:{imgCache:[],spacer:"/path/to/your/spacer.gif"},collate:function(aScope){var isOldIE=(document.all&&!window.opera&&!window.XDomainRequest)?1:0;if(isOldIE&&document.getElementsByTagName){var c=imgSizer;var imgCache=c.Config.imgCache;var images=(aScope&&aScope.length)?aScope:document.getElementsByTagName("img");for(var i=0;i<images.length;i++){images[i].origWidth=images[i].offsetWidth;images[i].origHeight=images[i].offsetHeight;imgCache.push(images[i]);c.ieAlpha(images[i]);images[i].style.width="100%";}
 if(imgCache.length){c.resize(function(){for(var i=0;i<imgCache.length;i++){var ratio=(imgCache[i].offsetWidth/imgCache[i].origWidth);imgCache[i].style.height=(imgCache[i].origHeight*ratio)+"px";}});}}},ieAlpha:function(img){var c=imgSizer;if(img.oldSrc){img.src=img.oldSrc;}
