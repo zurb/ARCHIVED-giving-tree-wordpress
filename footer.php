66858fd81d215d374314615199da4ea14ab55066
eddiemachado <machado08@gmail.com>
2011-09-23 18:13:39 -0500

    removed scripts and cleaned up code

--- footer.php
+++ footer.php
@@ -1,38 +1,28 @@
-			<footer role="contentinfo">
+<footer role="contentinfo">
 			
 				<div id="inner-footer" class="clearfix">
 					
-					<nav role="navigation">
+					<nav>
 						<?php bones_footer_links(); // Adjust using Menus in Wordpress Admin ?>
 					</nav>
 			
-					<p class="attribution">&copy; <?php bloginfo('name'); ?> <?php _e("is powered by", "bonestheme"); ?> <a href="http://wordpress.org/" title="WordPress">WordPress</a> <span class="amp">&</span> <a rel="nofollow" href="http://www.themble.com/bones" title="Bones" class="footer_bones_link">Bones</a>.</p>
+					<p class="attribution">&copy; <?php bloginfo('name'); ?> <?php _e("is powered by", "bonestheme"); ?> <a href="http://wordpress.org/" title="WordPress">WordPress</a> <span class="amp">&</span> <a href="http://www.themble.com/bones" title="Bones" class="footer_bones_link">Bones</a>.</p>
 				
 				</div> <!-- end #inner-footer -->
 				
 			</footer> <!-- end footer -->
 		
 		</div> <!-- end #container -->
-
-		<!-- custom scripts -->
+		
+		<!-- scripts are now optimized via Modernizr.load -->	
 		<script src="<?php echo get_template_directory_uri(); ?>/library/js/scripts.js"></script>
 		
-		<!--[if (lt IE 9) & (!IEMobile)]>
-			<script src="<?php echo get_template_directory_uri(); ?>/library/js/libs/ie/DOMAssistantCompressed-2.8.js"></script>
-			<script src="<?php echo get_template_directory_uri(); ?>/library/js/libs/ie/selectivizr-1.0.1.js"></script>
-			<script src="<?php echo get_template_directory_uri(); ?>/library/js/libs/respond.min.js"></script>
-		<![endif]-->		
-		
 		<!--[if lt IE 7 ]>
-    		<script src="<?php echo get_template_directory_uri(); ?>/library/js/libs/ie/dd_belatedpng.js"></script>
-    		<script>DD_belatedPNG.fix("img, .png_bg");</script>
-  		<![endif]-->
+  			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+  			<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+		<![endif]-->
 		
 		<?php wp_footer(); // js scripts are inserted using this function ?>
-		
-		<!-- Insert Analytics -->
-		
-		<!-- End Analytics -->
 
 	</body>
 
