7c032fd0f68f48838571549ed072af35faccc515
eddiemachado <machado08@gmail.com>
2011-10-29 10:29:45 -0500

    added new <data> and itemscope on posts
    just doing it on single and page for now, let's wait and see how it
    progresses…
    for more info:
    http://www.whatwg.org/specs/web-apps/current-work/multipage/text-level-s
    emantics.html#the-data-element

--- single.php
+++ single.php
@@ -6,17 +6,17 @@
 
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					
-					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
+					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 						
 						<header>
 							
-							<h1 class="single-title"><?php the_title(); ?></h1>
+							<h1 class="single-title" itemprop="headline"><?php the_title(); ?></h1>
 							
-							<p class="meta"><?php _e("Posted", "bonestheme"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time> <?php _e("by", "bonestheme"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "bonestheme"); ?> <?php the_category(', '); ?>.</p>
+							<p class="meta"><?php _e("Posted", "bonestheme"); ?> <data itemprop="datePublished" value="<?php echo the_time('Y-m-j'); ?>"><?php the_time('F jS, Y'); ?></data> <?php _e("by", "bonestheme"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "bonestheme"); ?> <?php the_category(', '); ?>.</p>
 						
 						</header> <!-- end article header -->
 					
-						<section class="post_content clearfix">
+						<section class="post_content clearfix" itemprop="articleBody">
 							<?php the_content(); ?>
 							
 					
