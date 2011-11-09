4477147be6b3db9e860e607b168c7d0782e9decc
eddiemachado <machado08@gmail.com>
2011-11-09 11:47:03 -0500

    adding site description
    it's commented out by default, but you can add it if you like

--- header.php
+++ header.php
@@ -59,8 +59,13 @@
 			<header role="banner">
 			
 				<div id="inner-header" class="clearfix">
-				
+					
+					<!-- to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
 					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					
+					<!-- if you'd like to use the site description you can un-comment it below -->
+					<?php // bloginfo('description'); ?>
+					
 					
 					<nav role="navigation">
 						<?php bones_main_nav(); // Adjust using Menus in Wordpress Admin ?>
