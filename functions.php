13d0e6b3998cb61c3b7ba8f2fd28a2b8972b4ec9
eddiemachado <machado08@gmail.com>
2011-12-01 12:11:05 -0500

    adding call to admin.php
    it's commented out by default

--- functions.php
+++ functions.php
@@ -13,6 +13,9 @@ require_once('library/bones.php');            // core functions (don't remove)
 require_once('library/plugins.php');          // plugins & extra functions (optional)
 require_once('library/custom-post-type.php'); // custom post type example
 
+// Admin Functions (commented out by default)
+// require_once('library/admin.php');         // custom admin functions
+
 /************* THUMBNAIL SIZE OPTIONS *************/
 
 // Thumbnail sizes
