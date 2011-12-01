739bcdeb71f26705f0d0a449d4e070c81bf9f4ad
eddiemachado <machado08@gmail.com>
2011-12-01 12:10:51 -0500

    removing p from around imgs
    http://css-tricks.com/snippets/wordpress/remove-paragraph-tags-from-arou
    nd-images/

--- library/bones.php
+++ library/bones.php
@@ -209,6 +209,15 @@ function page_navi($before = '', $after = '') {
 	}
 	echo '</ol></nav>'.$after."";
 }
+
+// remove the p from around imgs (http://css-tricks.com/snippets/wordpress/remove-paragraph-tags-from-around-images/)
+function filter_ptags_on_images($content){
+   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+
+add_filter('the_content', 'filter_ptags_on_images');
+
+
 	
 
 ?>
