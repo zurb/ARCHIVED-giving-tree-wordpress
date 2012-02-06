b95dc8cea29fb4cca743961dc2314aba06c4f5e1
polyvisual <matt@polyvisual.co.uk>
2012-02-06 20:06:40 +0000

    Fix for https://github.com/eddiemachado/bones/issues/47

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -35,3 +35,37 @@ $(document).ready(function() {
 
  
 }); /* end of as page load scripts */
+
+// HTML5 Fallbacks for older browsers
+$(function() {
+    // check placeholder browser support
+    if (!Modernizr.input.placeholder) {
+        // set placeholder values
+        $(this).find('[placeholder]').each(function() {
+            $(this).val( $(this).attr('placeholder') );
+        });
+ 
+        // focus and blur of placeholders
+        $('[placeholder]').focus(function() {
+            if ($(this).val() == $(this).attr('placeholder')) {
+                $(this).val('');
+                $(this).removeClass('placeholder');
+            }
+        }).blur(function() {
+            if ($(this).val() == '' || $(this).val() == $(this).attr('placeholder')) {
+                $(this).val($(this).attr('placeholder'));
+                $(this).addClass('placeholder');
+            }
+        });
+ 
+        // remove placeholders on submit
+        $('[placeholder]').closest('form').submit(function() {
+            $(this).find('[placeholder]').each(function() {
+                if ($(this).val() == $(this).attr('placeholder')) {
+                    $(this).val('');
+                }
+            });
+        });
+    }
+});
+
