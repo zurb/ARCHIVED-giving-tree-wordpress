e82cd10a898984ab6f77bfa35a84936bbfa9c994
eddiemachado <machado08@gmail.com>
2012-06-06 10:07:26 -0400

    updated boilerplate, cleaned up title tag, meta tags, removed query call, everything is inside a enqeueue

--- header.php
+++ header.php
@@ -1,54 +1,36 @@
 <!doctype html>  
 
-<!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
-<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6 oldie"> <![endif]-->
-<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7 oldie"> <![endif]-->
-<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8 oldie"> <![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+<!--[if IEMobile 7]><html <?php language_attributes(); ?> class="no-js iem7"><![endif]-->
+<!--[if (gt IEMobile 7)|!(IEMobile)]><html <?php language_attributes(); ?> class="no-js"><![endif]-->
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7 oldie"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 oldie"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><html <?php language_attributes(); ?> class="no-js"><![endif]-->
 	
 	<head>
 		<meta charset="utf-8">
+		
+		<title><?php wp_title(''); ?></title>
+		
+		<!-- Google Chrome Frame for IE -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		
-		<title><?php
-		/*
-		 * Print the <title> tag based on what is being viewed.
-		 */
-		global $page, $paged;
-		wp_title( '-', true, 'right' );
-		// Add the blog name.
-		bloginfo( 'name' );
-		// Add a page number if necessary:
-		if ( $paged >= 2 || $page >= 2 )
-			echo ' - ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
-		?></title>
-		
-		<meta name="description" content="">
-		<meta name="author" content="">
+		<!-- mobile meta (hooray!) -->
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		
 		<!-- icons & favicons (for more: http://themble.com/support/adding-icons-favicons/) -->
 		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-
-		<!-- default stylesheet -->
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/css/normalize.css">		
-		
-		<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-		<script>window.jQuery || document.write(unescape('%3Cscript src="<?php echo get_template_directory_uri(); ?>/library/js/libs/jquery-1.7.1.min.js"%3E%3C/script%3E'))</script>
-		
-		<!-- drop Google Analytics Here -->
-		<!-- end analytics -->
-		
-		<!-- modernizr -->
-		<script src="<?php echo get_template_directory_uri(); ?>/library/js/modernizr.full.min.js"></script>
-		
+				
   		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 		
 		<!-- wordpress head functions -->
 		<?php wp_head(); ?>
 		<!-- end of wordpress head -->
-		
-		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+			
+		<!-- drop Google Analytics Here -->
+		<!-- end analytics -->
 		
 	</head>
 	
@@ -56,9 +38,9 @@
 	
 		<div id="container">
 			
-			<header role="banner">
+			<header class="header" role="banner">
 			
-				<div id="inner-header" class="clearfix">
+				<div id="inner-header" class="wrap clearfix">
 					
 					<!-- to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
 					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
