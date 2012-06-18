87d70f1b694c077d2c89d088ddbb96b87a726d04
eddiemachado <machado08@gmail.com>
2012-06-18 09:33:45 -0400

    updated custom background code

--- library/bones.php
+++ library/bones.php
@@ -152,8 +152,16 @@ function bones_theme_support() {
 	// default thumb size   
 	set_post_thumbnail_size(125, 125, true);   
 	
-	// wp custom background
-	add_custom_background();        
+	// wp custom background (thx to @bransonwerner for update)
+	add_theme_support( 'custom-background',
+	    array( 
+	    'default-image' => '',  // background image default
+	    'default-color' => '', // background color default (dont add the #)
+	    'wp-head-callback' => '_custom_background_cb',
+	    'admin-head-callback' => '',
+	    'admin-preview-callback' => ''
+	    )
+	);      
 	
 	// rss thingy           
 	add_theme_support('automatic-feed-links'); 
