dff36afe81b5088e3a45c47119d8886cf0b89c18
eddiemachado <machado08@gmail.com>
2012-07-17 12:39:19 -0500

    added Get Computed Style fix for IE

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -9,6 +9,24 @@ slow the page load.
 
 */
 
+// IE8 ployfill for GetComputed Style (for Responsive Script below)
+if (!window.getComputedStyle) {
+    window.getComputedStyle = function(el, pseudo) {
+        this.el = el;
+        this.getPropertyValue = function(prop) {
+            var re = /(\-([a-z]){1})/g;
+            if (prop == 'float') prop = 'styleFloat';
+            if (re.test(prop)) {
+                prop = prop.replace(re, function () {
+                    return arguments[2].toUpperCase();
+                });
+            }
+            return el.currentStyle[prop] ? el.currentStyle[prop] : null;
+        }
+        return this;
+    }
+}
+
 // as the page loads, call these scripts
 jQuery(document).ready(function($) {
 
